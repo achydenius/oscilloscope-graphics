5f5e652c7d485940e510c3c9a2f7fae586381c74
Arto Chydenius <arto.chydenius@reaktor.com>
2020-10-03 21:44:08 +0300

    Separate clipping and rendering

--- OscilloscopeGraphics/Renderer.cpp
+++ OscilloscopeGraphics/Renderer.cpp
@@ -1,5 +1,4 @@
 #include "Renderer.h"
-#include <Arduino.h>
 
 // Transform point from canvas space to output space
 #define transform(p) ((uint32_t)(((p * 0.5) + 0.5) * maxValue))
@@ -8,120 +7,18 @@ using namespace osc;
 
 void Renderer::setWriteMode(DACWriteMode mode) { writeMode = mode; }
 
-void Renderer::setViewport(Viewport& vp) { viewport = vp; }
-
-void Renderer::drawPoint(float x, float y) {
-  if (x < viewport.left || x > viewport.right || y < viewport.bottom ||
-      y > viewport.top) {
-    return;
-  }
-
-  analogWrite(xPin, transform(x));
-  analogWrite(yPin, transform(y));
-}
-
 /*
- * Clip and draw a line
- *
- * Cohen-Sutherland line clipping algorithm implementation:
- * https://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
- */
-void Renderer::drawLine(float x0, float y0, float x1, float y1, bool clip) {
-  uint32_t c0 = getClipCode(x0, y0);
-  uint32_t c1 = getClipCode(x1, y1);
-  bool accept = false;
-
-  while (true) {
-    if (!clip) {
-      // No clipping - always accept
-      accept = true;
-      break;
-
-    } else if (!(c0 | c1)) {
-      // Both points inside window
-      accept = true;
-      break;
-
-    } else if (c0 & c1) {
-      // Both points outside window
-      break;
-
-    } else {
-      // One or both points need to be clipped
-      uint32_t c = c0 ? c0 : c1;
-      float x, y;
-
-      if (c & CLIP_TOP) {
-        // Point is above the clip window
-        x = x0 + (x1 - x0) * (viewport.top - y0) / (y1 - y0);
-        y = viewport.top;
-      } else if (c & CLIP_BOTTOM) {
-        // Point is below the clip window
-        x = x0 + (x1 - x0) * (viewport.bottom - y0) / (y1 - y0);
-        y = viewport.bottom;
-      } else if (c & CLIP_RIGHT) {
-        // Point is to the right of clip window
-        x = viewport.right;
-        y = y0 + (y1 - y0) * (viewport.right - x0) / (x1 - x0);
-      } else if (c & CLIP_LEFT) {
-        // Point is to the left of clip window
-        x = viewport.left;
-        y = y0 + (y1 - y0) * (viewport.left - x0) / (x1 - x0);
-      }
-
-      if (c == c0) {
-        x0 = x;
-        y0 = y;
-        c0 = getClipCode(x0, y0);
-      } else {
-        x1 = x;
-        y1 = y;
-        c1 = getClipCode(x1, y1);
-      }
-    }
-  }
-
-  if (accept) {
-    outputLine(transform(x0), transform(y0), transform(x1), transform(y1));
-  }
-}
-
-void Renderer::drawViewport() {
-  // Draw viewport
-  drawLine(-1.0, viewport.top, 1.0, viewport.top, false);
-  drawLine(-1.0, viewport.bottom, 1.0, viewport.bottom, false);
-  drawLine(viewport.left, 1.0, viewport.left, -1.0, false);
-  drawLine(viewport.right, 1.0, viewport.right, -1.0, false);
-
-  // Draw crosshair
-  drawLine(-1.0, 1.0, 1.0, -1.0, false);
-  drawLine(1.0, 1.0, -1.0, -1.0, false);
-}
-
-uint32_t Renderer::getClipCode(float x, float y) {
-  uint32_t code = CLIP_INSIDE;
-
-  if (x < viewport.left) {
-    code |= CLIP_LEFT;
-  } else if (x > viewport.right) {
-    code |= CLIP_RIGHT;
-  }
-  if (y < viewport.bottom) {
-    code |= CLIP_BOTTOM;
-  } else if (y > viewport.top) {
-    code |= CLIP_TOP;
-  }
-
-  return code;
-}
-
-/*
- * Output a line
+ * Draw a line
  *
  * DDA line drawing algorithm implementation:
  * https://www.geeksforgeeks.org/dda-line-generation-algorithm-computer-graphics/
  */
-void Renderer::outputLine(uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1) {
+void Renderer::drawLine(vec2& a, vec2& b) {
+  int32_t x0 = transform(a[0]);
+  int32_t y0 = transform(a[1]);
+  int32_t x1 = transform(b[0]);
+  int32_t y1 = transform(b[1]);
+
   int32_t dx = x1 - x0;
   int32_t dy = y1 - y0;
 
--- OscilloscopeGraphics/Renderer.h
+++ OscilloscopeGraphics/Renderer.h
@@ -4,6 +4,8 @@
 #include <Arduino.h>
 #include <math.h>
 
+#include "src/cglm/include/cglm/cglm.h"
+
 namespace osc {
 class Renderer {
  public:
@@ -11,43 +13,27 @@ class Renderer {
   // Adafruit Metro M4 (https://www.adafruit.com/product/3382) as they directly
   // access the low-level API.
   enum DACWriteMode { STANDARD, INLINE, DIRECT };
-  struct Viewport {
-    float top, bottom, left, right;
-  };
 
  private:
-  const uint32_t CLIP_INSIDE = 0;
-  const uint32_t CLIP_LEFT = 1;
-  const uint32_t CLIP_RIGHT = 2;
-  const uint32_t CLIP_BOTTOM = 4;
-  const uint32_t CLIP_TOP = 8;
-
   const uint8_t resolution;
   const uint8_t xPin, yPin;
   uint32_t maxValue;
   DACWriteMode writeMode;
-  Viewport viewport;
 
  public:
   Renderer(uint8_t resolution, uint8_t xPin, uint8_t yPin, DACWriteMode mode)
       : resolution(resolution), xPin(xPin), yPin(yPin), writeMode(mode) {
     analogWriteResolution(resolution);
     maxValue = pow(2, resolution) - 1;
-    viewport = {1.0, -1.0, -1.0, 1.0};
 
     // Initialize DAC manually (needed by direct write modes)
     analogWrite(xPin, 0);
     analogWrite(yPin, 0);
   }
   void setWriteMode(DACWriteMode mode);
-  void setViewport(Viewport& vp);
-  void drawPoint(float x, float y);
-  void drawLine(float x0, float y0, float x1, float y1, bool clip = true);
-  void drawViewport();
+  void drawLine(vec2& a, vec2& b);
 
  private:
-  void outputLine(uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1);
-  uint32_t getClipCode(float x, float y);
   void dacWriteAnalogWrite(uint32_t x, uint32_t y);
   void dacWriteInline(uint32_t x, uint32_t y, uint32_t shift);
   void dacWriteDirect(uint32_t x, uint32_t y, uint32_t shift);
