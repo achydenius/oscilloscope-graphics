fa59aba7ddc16ac6d0f0a329e42b9923cdba13d5
Arto Chydenius <arto.chydenius@reaktor.com>
2020-11-23 21:16:50 +0200

    Update example program to draw a 3D icosahedron

--- example.py
+++ example.py
@@ -1,37 +1,74 @@
 from client import Client
-from math import pi, sin, cos
+from math import pi, sin, sqrt
 from time import sleep
+from pyrr import Vector3, Matrix44
 
 SERIAL_PORT_NAME = '/dev/cu.usbmodem1432201'
 SERIAL_PORT_BAUDRATE = 115200
 
 
-def generate_circle_lines(vertex_count, radius, offset=0):
-    # Create vertices
-    vertices = []
-    for i in range(0, vertex_count):
-        rad = 2 * pi / vertex_count * i
-        vertices.append((cos(rad + offset) * radius,
-                         sin(rad + offset) * radius))
+def create_icosahedron():
+    x = 0.525731112119133606
+    z = 0.850650808352039932
 
-    # Create lines from vertices
-    lines = []
-    for i in range(0, vertex_count):
-        lines.append(
-            (vertices[i], vertices[(i + 1) % vertex_count])
-        )
+    return {
+        'vertices': [
+            Vector3([-x, 0, z]), Vector3([x, 0, z]), Vector3([-x, 0, -z]), Vector3([x, 0, -z]),
+            Vector3([0, z, x]), Vector3([0, z, -x]), Vector3([0, -z, x]), Vector3([0, -z, -x]),
+            Vector3([z, x, 0]), Vector3([-z, x, 0]), Vector3([z, -x, 0]), Vector3([-z, -x, 0])
+        ],
+        'faces': [
+            [0, 4, 1], [0, 9, 4], [9, 5, 4], [4, 5, 8], [4, 8, 1],
+            [8, 10, 1], [8, 3, 10], [5, 3, 8], [5, 2, 3], [2, 7, 3],
+            [7, 10, 3], [7, 6, 10], [7, 11, 6], [11, 0, 6], [0, 1, 6],
+            [6, 1, 10], [9, 0, 11], [9, 11, 2], [9, 2, 5], [7, 2, 11]
+        ]
+    }
 
-    return lines
+
+def find_edges(faces):
+    edges = []
+    existing = set()
+
+    for face in faces:
+        for i in range(len(face)):
+            edge = (face[i], face[(i + 1) % len(face)])
+            s = frozenset(edge)
+            if s not in existing:
+                existing.add(s)
+                edges.append(edge)
+
+    return edges
+
+
+def edges_to_lines(vertices, edges):
+    return [((vertices[edge[0]][0], vertices[edge[0]][1]),
+             (vertices[edge[1]][0], vertices[edge[1]][1])) for edge in edges]
 
 
 if __name__ == '__main__':
     Client.print_ports()
     client = Client(SERIAL_PORT_NAME, SERIAL_PORT_BAUDRATE)
 
+    mesh = create_icosahedron()
     phase = 0
     while True:
-        lines = generate_circle_lines(8, 0.75, phase)
+        # Create transformation matrix
+        translation = Matrix44.from_translation([sin(phase), 0, 5.0])
+        rotation = Matrix44.from_eulers([0, phase, phase * 0.5])
+        projection = Matrix44.perspective_projection(45.0, 1.0, 0.01, 100.0)
+        matrix = projection * translation * rotation
+
+        # Transform vertices
+        vertices = [matrix * vertex for vertex in mesh['vertices']]
+
+        # Find unique edges
+        edges = find_edges(mesh['faces'])
+
+        # Convert edges to lines in client format
+        lines = edges_to_lines(vertices, edges)
+
         client.send(lines)
-        phase += 0.01
+        phase += 0.02
 
         sleep(0.01)
